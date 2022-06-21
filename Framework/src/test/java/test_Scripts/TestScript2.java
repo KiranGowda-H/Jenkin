c6d6e23346b46db6e65fcc562af18257d2e8a4f7
KIRAN GOWDA <KIRAN GOWDA@ELF34-KiranGowdaH>
2022-06-21 16:16:02 +0530

    push

---
+++ Framework/src/test/java/test_Scripts/TestScript2.java
@@ -0,0 +1,41 @@
+package test_Scripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import generic.Base_Test;
+import generic.ReadExcel;
+import pom_scripts.RegisterPage;
+import pom_scripts.WelcomePage;
+
+public class TestScript2 extends Base_Test{
+	@Test(dataProvider = "testdata")
+	public void testScript2(String male, String firstname, String lastname, String email, String password, String confirmpassword) throws IOException, InterruptedException {
+		  WelcomePage welcomepage=new WelcomePage(driver);
+		  welcomepage.clickRegister();
+		  RegisterPage registerPage=new RegisterPage(driver);
+		  if(male.equalsIgnoreCase("M")){
+			  registerPage.clickGenderMaleRadioButton();
+		  }else{
+			  registerPage.clickGenderFemaleRadioButton();
+		  }
+		  registerPage.firstnameTextfield(firstname);
+		  registerPage.lastnameTextField(lastname);
+		  registerPage.email_value(email);
+		  registerPage.password_value(password);
+		  registerPage.confirmpassword_value(confirmpassword);
+		  registerPage.registerButton();
+		  test.log(LogStatus.INFO, test.addScreenCapture(getPhoto(driver))); 
+		  test.log(LogStatus.INFO, "User is logged in");
+	}
+	
+	@DataProvider(name = "testdata")
+	public Object[][] getTestData() throws IOException {
+		Object [][]testdata=ReadExcel.getMultipleData("Register");
+		return testdata;
+	}
+}
---
+++ Framework/src/test/java/test_Scripts/TestScript24.java
@@ -0,0 +1,64 @@
+package test_Scripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import generic.Base_Test;
+import generic.ReadExcel;
+import pom_scripts.Books;
+import pom_scripts.HomePage;
+import pom_scripts.LoginPage;
+import pom_scripts.ProductPage;
+import pom_scripts.WelcomePage;
+
+public class TestScript24 extends Base_Test{
+	@Test(dataProvider = "testdata")
+	public void testScript24(String email, String pwd) throws IOException, InterruptedException {
+		  WelcomePage welcomePage=new WelcomePage(driver);
+		  welcomePage.clickLogin();
+		  LoginPage loginPage=new LoginPage(driver);
+		  loginPage.enterValueInEmail(email);
+		  loginPage.enterValueInPwd(pwd); 
+		  loginPage.clickLoginButton();
+		 /* welcomePage.clickRegister();
+		  RegisterPage registerPage=new RegisterPage(driver);
+		  if(male.equalsIgnoreCase("M")){
+			  registerPage.clickGenderMaleRadioButton();
+		  }else{
+			  registerPage.clickGenderFemaleRadioButton();
+		  }
+		  registerPage.firstnameTextfield(firstname);
+		  registerPage.lastnameTextField(lastname);
+		  registerPage.email_value(email);
+		  registerPage.password_value(password);
+		  registerPage.confirmpassword_value(confirmpassword);
+		  registerPage.registerButton();
+		  registerPage.continueButton();*/
+		  test.log(LogStatus.INFO, "User is logged in");
+		  HomePage homePage=new HomePage(driver);
+		  homePage.booksButton();
+		  Books booksPage=new Books(driver);
+		  booksPage.sortByDropdown();
+		  booksPage.displayPerPageDropdown();
+		  booksPage.viewAsDropdown();
+		  booksPage.filter25to50();
+		  booksPage.removeFilter();
+		  booksPage.fictionExBookPage();
+		  ProductPage productPage=new ProductPage(driver);
+		  productPage.addToComparelistbutton();
+		  homePage.booksButton();
+		  booksPage.fictionExBookPage();
+		  Thread.sleep(20000);
+		  booksPage.twitterIcon();
+		  test.log(LogStatus.INFO, test.addScreenCapture(getPhoto(driver))); 
+}
+	@DataProvider(name = "testdata")
+	public Object[][] getTestData() throws IOException {
+		Object [][]testdata=ReadExcel.getMultipleData("Login");
+		return testdata;
+	}
+}
